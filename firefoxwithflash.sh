fd9ba7b60c2634afddf392eeccda6750cc515cb7
xxliao <xxliao@users.noreply.github.com>
2016-12-27 21:44:14 +0800

    install firefox with flash

---
+++ firefoxwithflash.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/sh
+#get firefox
+wget http://ftp.mozilla.org/pub/firefox/releases/45.6.0esr/linux-i686/en-US/firefox-45.6.0esr.tar.bz2
+#get flash player
+wget http://fpdownload.macromedia.com/get/flashplayer/pdc/24.0.0.186/flash_player_npapi_linux.i386.tar.gz
+mkdir /root/Downloads/flashplayer
+tar -zxvf flash_player_npapi_linux.i386.tar.gz -C /root/Downloads/flashplayer
+tar -xjvf firefox-45.6.0esr.tar.bz2 -C /root/Downloads/
+cp /root/Downloads/flashplayer/libflashplayer.so /root/Downloads/firefox/plugins/libflashplayer.so
+mv /root/Downloads/firefox/ /usr/local/lib/
+ln -s /usr/local/lib/firefox/firefox /usr/bin/firefox
