b9bd972fad2e03210766179168eff88f18ec02b9
xxliao <xxliao@users.noreply.github.com>
2016-12-27 23:19:21 +0800

    update plugins

--- firefoxwithflash.sh
+++ firefoxwithflash.sh
@@ -3,10 +3,10 @@
 wget http://ftp.mozilla.org/pub/firefox/releases/45.6.0esr/linux-i686/en-US/firefox-45.6.0esr.tar.bz2
 #get flash player
 wget http://fpdownload.macromedia.com/get/flashplayer/pdc/24.0.0.186/flash_player_npapi_linux.i386.tar.gz
-mkdir /root/Downloads/flashplayer
+mkdir -p /root/Downloads/flashplayer
 tar -zxvf flash_player_npapi_linux.i386.tar.gz -C /root/Downloads/flashplayer
 tar -xjvf firefox-45.6.0esr.tar.bz2 -C /root/Downloads/
-mkdir /root/Downloads/firefox/plugins/
-cp /root/Downloads/flashplayer/libflashplayer.so /root/Downloads/firefox/plugins/libflashplayer.so
+mkdir -p /root/Downloads/firefox/browser/plugins/
+cp /root/Downloads/flashplayer/libflashplayer.so /root/Downloads/firefox/browser/plugins/libflashplayer.so
 mv /root/Downloads/firefox/ /usr/local/lib/
 ln -s /usr/local/lib/firefox/firefox /usr/bin/firefox
