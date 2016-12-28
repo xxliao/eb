313f09ef0f79c2b35a0adbcb6bd32b78d9e32f8e
xxliao <xxliao@users.noreply.github.com>
2016-12-28 12:52:09 +0800

    change firefox to 35.0.1

--- firefoxwithflash.sh
+++ firefoxwithflash.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 #get firefox
-wget http://ftp.mozilla.org/pub/firefox/releases/45.6.0esr/linux-i686/en-US/firefox-45.6.0esr.tar.bz2
+wget http://ftp.mozilla.org/pub/firefox/releases/35.0.1/linux-i686/en-US/firefox-35.0.1.tar.bz2
 #get flash player
 wget http://fpdownload.macromedia.com/get/flashplayer/pdc/24.0.0.186/flash_player_npapi_linux.i386.tar.gz
 mkdir -p /root/Downloads/flashplayer
 tar -zxvf flash_player_npapi_linux.i386.tar.gz -C /root/Downloads/flashplayer
-tar -xjvf firefox-45.6.0esr.tar.bz2 -C /root/Downloads/
+tar -xjvf firefox-35.0.1.tar.bz2 -C /root/Downloads/
 mkdir -p /root/Downloads/firefox/browser/plugins/
 cp /root/Downloads/flashplayer/libflashplayer.so /root/Downloads/firefox/browser/plugins/libflashplayer.so
 mv /root/Downloads/firefox/ /usr/local/lib/
 ln -s /usr/local/lib/firefox/firefox /usr/bin/firefox
+
