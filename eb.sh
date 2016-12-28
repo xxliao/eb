dd377683a4628b362bb3454839ae561b9aee39d5
xxliao <xxliao@users.noreply.github.com>
2016-12-28 10:18:13 +0800

    change firefox to 30.1.8

--- eb.sh
+++ eb.sh
@@ -1,17 +1,22 @@
 ﻿#!/bin/sh
 apt-get update
 apt-get -q -y --force-yes install xorg lxde-core tightvncserver xterm jwm mercurial libasound2-dev libcurl4-openssl-dev libnotify-dev libxt-dev libiw-dev mesa-common-dev autoconf2.13 yasm libidl-dev screen unzip bzip2 nano
+dpkg-reconfigure locales
+apt-get install ttf-arphic-ukai ttf-arphic-uming ttf-arphic-gbsn00lp ttf-arphic-bkai00mp ttf-arphic-bsmi00lp
 #get firefox
-wget http://ftp.mozilla.org/pub/firefox/releases/45.6.0esr/linux-i686/en-US/firefox-45.6.0esr.tar.bz2
+wget http://ftp.mozilla.org/pub/firefox/releases/31.8.0esr/linux-i686/en-US/firefox-31.8.0esr.tar.bz2
 #get flash player
 wget http://fpdownload.macromedia.com/get/flashplayer/pdc/24.0.0.186/flash_player_npapi_linux.i386.tar.gz
-mkdir /root/Downloads/flashplayer
+mkdir -p /root/Downloads/flashplayer
 tar -zxvf flash_player_npapi_linux.i386.tar.gz -C /root/Downloads/flashplayer
 tar -xjvf firefox-45.6.0esr.tar.bz2 -C /root/Downloads/
-mkdir /root/Downloads/firefox/plugins/
-cp /root/Downloads/flashplayer/libflashplayer.so /root/Downloads/firefox/plugins/libflashplayer.so
+mkdir -p /root/Downloads/firefox/browser/plugins/
+cp /root/Downloads/flashplayer/libflashplayer.so /root/Downloads/firefox/browser/plugins/libflashplayer.so
 mv /root/Downloads/firefox/ /usr/local/lib/
 ln -s /usr/local/lib/firefox/firefox /usr/bin/firefox
+
+
+wget http://atomstar.eu.org/eb/vncxstartup.txt http://atomstar.eu.org/eb/vncserverinit.txt
 
 vncserver
 rm -f ~/.vnc/xstartup
