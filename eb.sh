68ee1438831bb7c2a2f632a1cefac75c7a9f7ec3
xxliao <xxliao@users.noreply.github.com>
2016-12-31 12:33:55 +0800

    add firefox restart function

--- eb.sh
+++ eb.sh
@@ -1,6 +1,6 @@
 ﻿#!/bin/sh
 apt-get update
-apt-get -q -y --force-yes install xorg lxde-core tightvncserver xterm jwm mercurial libasound2-dev libcurl4-openssl-dev libnotify-dev libxt-dev libiw-dev mesa-common-dev autoconf2.13 yasm libidl-dev screen unzip bzip2 nano
+apt-get -q -y --force-yes install screen xorg lxde-core tightvncserver xterm jwm mercurial libasound2-dev libcurl4-openssl-dev libnotify-dev libxt-dev libiw-dev mesa-common-dev autoconf2.13 yasm libidl-dev screen unzip bzip2 nano
 dpkg-reconfigure locales
 apt-get install ttf-arphic-ukai ttf-arphic-uming ttf-arphic-gbsn00lp ttf-arphic-bkai00mp ttf-arphic-bsmi00lp
 #get firefox
@@ -15,8 +15,9 @@ cp /root/Downloads/flashplayer/libflashplayer.so /root/Downloads/firefox/browser
 mv /root/Downloads/firefox/ /usr/local/lib/
 ln -s /usr/local/lib/firefox/firefox /usr/bin/firefox
 
+wget https://raw.githubusercontent.com/xxliao/eb/master/testFF.sh
 
-wget http://atomstar.eu.org/eb/vncxstartup.txt http://atomstar.eu.org/eb/vncserverinit.txt
+wget https://raw.githubusercontent.com/xxliao/eb/master/vncxstartup.txt https://raw.githubusercontent.com/xxliao/eb/master/vncserverinit.txt
 
 vncserver
 rm -f ~/.vnc/xstartup
@@ -24,4 +25,8 @@ mv vncxstartup.txt ~/.vnc/xstartup
 chmod +x ~/.vnc/xstartup
 mv vncserverinit.txt /etc/init.d/vncserver
 chmod +x /etc/init.d/vncserver
+mv testFF.sh /etc/init.d/testFF
+chmod +x /etc/init.d/testFF
 update-rc.d vncserver defaults
+update-rc.d testFF defaults
+
