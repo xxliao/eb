d2f6c2f8d8579447ed051a8ba5bcf6f9a3b356b3
xxliao <xxliao@users.noreply.github.com>
2017-03-10 16:28:43 +0800

    安装flash

---
+++ installflash.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+
+mkdir /root/Downloads/
+mkdir /root/Downloads/flash
+
+wget https://fpdownload.adobe.com/pub/flashplayer/pdc/24.0.0.221/flash-player-npapi-24.0.0.221-release.x86_64.rpm
+
+rpm -ivh /root/Downloads/flash/flash-player-npapi-24.0.0.221-release.x86_64.rpm
+
